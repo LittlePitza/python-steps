d0a77d70e2a6e8e03c5d01a91d032bf9eea92966
Pitza <luishc23@icloud.com>
2025-01-23 17:33:10 -0600

    Add a contact management system to practice project
    Implemented functions to add, search, display, and delete contacts. Included a main menu interface for user interaction. This enhances the project with practical utilities for managing contact information.

---
+++ automateBoringStuff-WithPython/Part 1_Basics/CH5_Dictionaries/practice_projects/contacts_.py
@@ -0,0 +1,63 @@
+contacts = {
+    # Default values
+        'Support': {'name':'Support', 'phone': '091', 'country': 'Iran'},
+}
+
+def add_contact(name, phone, country):
+    contacts[name] = {'phone': phone, 'country': country}
+
+def search_contact(name):
+    if name in contacts:
+        for key, value in contacts[name].items():
+            print(key + ': ' + value)
+    else:
+        print('Contact not found')
+
+def show_all_contacts():
+    for name, contact in contacts.items():
+        print(name)
+        for key, value in contact.items():
+            print('\t' + key + ': ' + value)
+
+def delete_contact(name):
+    if name in contacts:
+        choise = input('Are you sure you want to delete ' + name + '? (y/n) ')
+        if choise == 'y':
+            del contacts[name]
+            print('Contact deleted')
+        else:
+            print('Contact not deleted')
+    else:
+        print('Contact not found')
+
+def main():
+    while True:
+        print('Contact Support number: ' + contacts[ 'Support'][ 'phone'])
+        print('1. Add contact')
+        print('2. Search contact')
+        print('3. Show all contacts')
+        print('4. Delete contact')
+        print('5. Exit')
+        choise = input('Enter your choise: ')
+        if choise == '1':
+            name = input('Enter contact name: ')
+            phone = input('Enter contact phone: ')
+            country = input('Enter contact country: ')
+            add_contact(name, phone, country)
+
+        elif choise == '2':
+            name = input('Enter contact name: ')
+            search_contact(name)
+
+        elif choise == '3':
+            show_all_contacts()
+
+        elif choise == '4':
+            name = input('Enter contact name: ')
+            delete_contact(name)
+
+        elif choise == '5':
+            exit()
+
+if __name__ == '__main__':
+    main()
