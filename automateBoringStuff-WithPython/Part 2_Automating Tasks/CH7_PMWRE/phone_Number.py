044bcc72b5418fe978b7b03d7c4dbe6479fda39c
Pitza <luishc23@icloud.com>
2025-01-23 19:03:39 -0600

    Add phone number regex validation script
    This script uses regular expressions to validate and extract phone numbers from user input. It accepts numbers in formats with or without dashes and provides appropriate feedback.

---
+++ automateBoringStuff-WithPython/Part 2_Automating Tasks/CH7_PMWRE/phone_Number.py
@@ -0,0 +1,16 @@
+import re
+
+
+phoneNumber_Regex = re.compile(r'(\d{3})(-)?(\d{3})(-)?(\d{4})')
+mo = phoneNumber_Regex.search(input('Enter a phone number: '))
+
+if mo:
+    print('Phone number found: ' + mo.group(0))
+    # Verification
+    if mo.group(2) and mo.group(4):
+        print(f'Phone number: {mo.group(1)}-{mo.group(3)}-{mo.group(5)}')
+
+    else:
+        print(f'Phone number: {mo.group(1)} {mo.group(3)} {mo.group(5)}')
+else:
+    print('No phone number found.')
